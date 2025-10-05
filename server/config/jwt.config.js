96f9ad449eafa9be66ff81570115666254950152
Christopher Ivy <CHRISIVY22703@GMAIL.COM>
2025-10-05 12:33:06 -0500

    Created V1.0.0 Project Routes/CRUD #20 #9

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 const { verify } = jwt;
 const secret = process.env.SECRET_KEY;
 
+//middleware to authenticate user when requests are made
 export function authenticate(req, res, next) {
     console.log("config/jwt.config.js - Line 5", req.cookies);
     verify(req.cookies.userToken, secret, (err, payload) => {
@@ -9,6 +10,8 @@ export function authenticate(req, res, next) {
             res.status(401).json({ verified: false });
         } else {
             console.log("config/jwt.config.js - Line 11", payload);
+            // if user is authenticated add the user ID to the request object so that it can be accessed in the routes as needed
+            req.body.userId = payload._id;
             next();
         }
     });
--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,6 +1,9 @@
 import {
     addProject,
     findAllProjects,
+    findOneProject,
+    updateProject,
+    deleteProject,
 } from "../controllers/project.controller.js";
 import { authenticate } from "../config/jwt.config.js";
 
@@ -16,5 +19,23 @@ export default (app) => {
         "/api/projects",
         // authenticate, //! Disabled for DEV purposes ENABLE BEFORE LAUNCH
         findAllProjects
+    );
+    //Get one Project
+    app.get(
+        "/api/project/:id",
+        // authenticate, //! Disabled for DEV purposes ENABLE BEFORE LAUNCH
+        findOneProject
+    );
+    //Update a Project
+    app.patch(
+        "/api/project/update/:id",
+        // authenticate, //! Disabled for DEV purposes ENABLE BEFORE LAUNCH
+        updateProject
+    );
+    // Delete a Project
+    app.delete(
+        "/api/project/delete/:id",
+        // authenticate, //! Disabled for DEV purposes ENABLE BEFORE LAUNCH
+        deleteProject
     );
 };
