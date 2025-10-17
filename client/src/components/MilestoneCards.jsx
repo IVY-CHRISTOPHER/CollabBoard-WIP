8077ffaa97c4ad92b39481e1db3b697d687a517a
Sapphire Sandoval <sapphire.sandoval@gmail.com>
2025-10-17 10:48:30 -0600

    Create Project Dashboard Fixes #16

--- client/src/components/MilestoneCards.jsx
+++ client/src/components/MilestoneCards.jsx
@@ -1,28 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { userContext } from '../context/userContext'
+import {Link, useNavigate, useParams} from 'react-router-dom';
+import axios from 'axios';
 
 const MilestoneCards = (props) => {
+    const {project_id} = useParams()
     const { milestones, setMilestones } = useContext(userContext)
 
-    const allmilestones = [
-        { 'id': '1', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '2', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '3', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '4', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '5', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '6', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '7', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '8', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '9', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '10', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '11', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '12', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '13', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-        { 'id': '14', 'milestoneName': 'Name', 'tasks': [{'id' : '1', 'task' : 'task1'}, {'id' : '2', 'task' : 'task1'}, {'id' : '3', 'task' : 'task1'}, {'id' : '4', 'task' : 'task1'}, {'id' : '5', 'task' : 'task1'}, {'id' : '6', 'task' : 'task1'}, {'id' : '7', 'task' : 'task1'}, {'id' : '8', 'task' : 'task'},{'id' : '9', 'task' : 'task9'}, {'id' : '10', 'task' : 'task10'}] },
-    ]
-
     useEffect(() => {
-        setMilestones(allmilestones)
+        axios.get('http://localhost:3000/api/milestones', {withCredentials: true})
+            .then(res => setMilestones(res.data))
+            .catch(err => console.log(err))
     }, [])
 
 
@@ -38,14 +26,20 @@ const MilestoneCards = (props) => {
                             <div>
                                 {milestone.tasks.map(task => (
                                     <div key={task.id}>
-                                        <input type="checkbox" name="" id="" className='appearance-none rounded-full border border-black w-3 h-3 checked:bg-midnightBlu' />
-                                        <label htmlFor="">{task.task}</label>
+                                        <input type="checkbox" name="task" id="task" className='appearance-none rounded-full border border-black w-3 h-3 checked:bg-midnightBlu' />
+                                        <label htmlFor="task">{task.task}</label>
                                     </div>
                                 ))}
                             </div>
                         </div>
                     ))
                 }
+                <div className='w-[200px] flex-shrink-0 rounded-md ml-4 flex flex-col justify-center items-center'>
+                    <Link to={`/project/${project_id}/milestone/create`} className='flex flex-col justify-center items-center'>
+                        <img src="\src\assets\PlusGryIcon.svg" alt="Add Icon"/>
+                        <p className='text-aluminium'>Milestone</p>
+                    </Link>
+                </div>
             </div>
         </div>
     )
--- client/src/components/ProgressTiles.jsx
+++ client/src/components/ProgressTiles.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { userContext } from '../context/userContext'
 
 const ProgressTiles = (props) => {
+    const { milestones, setMilestones } = useContext(userContext)
 
-    
-    
+    const milestoneCount = milestones.length
+    console.log(milestoneCount)
+    console.log(milestones)
+
     return (
         <div className='flex gap-4 m-3'>
             <div className='flex bg-electricBlu rounded-md w-1/4 text-white items-start mt-2 p-2'>
                 <img src="\src\assets\TasksIcon.svg" alt="Task Icon" className='mt-1 mr-2'/>
                 <div>
-                    <p className='text-lg'> # Total Milestones</p>
+                    <p className='text-lg'> {milestoneCount} Total Milestones</p>
                     <p className='text-sm'>Active Milestones</p>
                 </div>
             </div>
             <div className='flex bg-lemonLt rounded-md w-1/4 text-black items-start mt-2 p-2'>
                 <img src="\src\assets\ProgressIcon.svg" alt="Progress Icon" className='mt-1 mr-2'/>
                 <div>
-                    <p className='text-lg'> # In Progress</p>
+                    <p className='text-lg'> 0 In Progress</p>
                     <p className='text-sm'>Currently Active</p>
                 </div>
             </div>
             <div className='flex bg-magicMint rounded-md w-1/4 text-black items-start mt-2 p-2'>
                 <img src="\src\assets\CompleteIcon.svg" alt="Complete Icon" className='mt-1 mr-2'/>
                 <div>
-                    <p className='text-lg'> # Complete</p>
-                    <p className='text-sm'>#% Complete</p>
+                    <p className='text-lg'> 0 Complete</p>
+                    <p className='text-sm'> 0% Complete</p>
                 </div>
             </div>
             <div className='flex bg-mistyRose rounded-md w-1/4 text-black items-start mt-2 p-2'>
                 <img src="\src\assets\WarningIcon.svg" alt="Warning Icon" className='mt-1 mr-2'/>
                 <div>
-                    <p className='text-lg'> # Overdue</p>
+                    <p className='text-lg'> 0 Overdue</p>
                     <p className='text-sm'>needs Attention</p>
                 </div>
             </div>
